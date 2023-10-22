dcc1762341c4820b71d385a931be0b399c268f65
abjadagency <abjadagencylimited@gmail.com>
2023-10-22 07:50:14 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function updateCountdown() {
+    const eventDate = new Date("2024-03-01T00:00:00").getTime();
+    const currentDate = new Date().getTime();
+    const timeRemaining = eventDate - currentDate;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = formatTime(days);
+    document.getElementById("hours").textContent = formatTime(hours);
+    document.getElementById("minutes").textContent = formatTime(minutes);
+    document.getElementById("seconds").textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
+
+// Initial call to display the countdown immediately
+updateCountdown();
